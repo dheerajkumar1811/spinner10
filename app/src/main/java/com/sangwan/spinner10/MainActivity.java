d9a1da06a7e46d01e103cf3afef69782419965ca
Dheeraj Kumar <dheeraj181195@gmail.com>
2018-11-17 10:45:39 +0530

    Initial commit

---
+++ app/src/main/java/com/sangwan/spinner10/MainActivity.java
@@ -0,0 +1,44 @@
+package com.sangwan.spinner10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    AdapterView.OnItemSelectedListener
+
+    {
+        String[] country = {"India", "USA", "China", "Japan", "Other"};
+
+        @Override
+        protected void onCreate (Bundle Bundle savedInstanceState;
+        savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+        spin.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa;
+        aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, country);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+
+    }
+        //Performing action onItemSelected and onNothing selected
+        @Override
+        public void onItemSelected Object arg0;
+        (AdapterView < ? > arg0, View arg1,int position, long id){
+        Toast.makeText(getApplicationContext(), country[position], Toast.LENGTH_LONG).show();
+    }
+        @Override
+        public void onNothingSelected (AdapterView < ? > arg0){
+        // TODO Auto-generated method stub
+
+    }
+    }}
